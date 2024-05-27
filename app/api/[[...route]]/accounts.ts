7fc0fcd291945c2d6d795b2977e21c8073f2a811
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2024-05-27 17:31:40 -0300

    cleaner api error handling

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,7 +1,6 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
 
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
@@ -10,9 +9,7 @@ const app = new Hono().get("/", clerkMiddleware(), async (c) => {
   const auth = getAuth(c);
 
   if (!auth?.userId) {
-    throw new HTTPException(401, {
-      res: c.json({ error: "Unauthorized" }, 401),
-    });
+    return c.json({ error: "Unauthorized" }, 401);
   }
 
   const data = await db
